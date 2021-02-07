1427861f6fd0fab9ecc9b8bb4c191a30d2c4d747
Julian Rosse <julian@helixbass.net>
2021-02-07 12:49:06 -0500

    branchIfFailsPredicate()

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 export {default as branchIfNullish} from './branchIfNullish'
 export {default as branchIfFalsy} from './branchIfFalsy'
 export {default as branchIfEmpty} from './branchIfEmpty'
+export {default as branchIfFailsPredicate} from './branchIfFailsPredicate'
 export {default as addPropTrackingRef} from './addPropTrackingRef'
 export {default as addPropIdentityStabilization} from './addPropIdentityStabilization'
 export {default as cleanupProps} from './cleanupProps'
